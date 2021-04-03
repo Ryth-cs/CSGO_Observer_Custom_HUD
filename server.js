55beff760091b8fbbe010db7f0511ef741e294ec
Ryth <rts512@york.ac.uk>
2021-04-04 00:20:58 +0100

    Migrated to Saas and restructured Scoreline CSS

--- server.js
+++ server.js
@@ -1,33 +1,27 @@
-var express = require('express');
+const express = require('express');
 var app = express();
 var server = app.listen(1338);
+const fs = require('fs');
 
 app.use(express.static('public'));
 // app.use(express.static('game_int_testing'));
 
-const fs = require('fs');
-// while (true) {
-//     setTimeout( function () {
-//         let rawdata = JSON.parse(fs.readFileSync('game_int_testing/test_json/0.json'));
-//         console.log(rawdata);
-//     }, 1000);
+
+// Testing cycle for development
+// var file_counter = 0
+// function read_send_data() {
+//     let rawdata = JSON.parse(fs.readFileSync(`game_int_testing/test_json/${file_counter}.json`));
+
+//     console.log(rawdata);
+//     send_data(rawdata);
+
+//     if (file_counter == 7) {file_counter = 0} else {file_counter += 1}
 // }
-var file_counter = 0
-function read_send_data() {
-    if (file_counter == 8) {
-        file_counter = 0
-    }
-    let rawdata = JSON.parse(fs.readFileSync(`game_int_testing/test_json/${file_counter}.json`));
+// setInterval(read_send_data, 1000);
 
-    console.log(rawdata);
-    send_data(rawdata);
 
-    file_counter += 1
-}
-setInterval(read_send_data, 1000);
-
+// Socket.io configuration
 var socketio = require('socket.io');
-
 var socket = socketio(server);
 
 socket.on('connection', function(socket) {
@@ -39,29 +33,30 @@ function send_data(data) {
 }
 
 
-const http2 = require('http'),
-cs_listener = http2.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  if (req.method != "POST") {
-      return res.end('');
-  }
-  let body,
-      data,
-      bodyChunks = [];
-  req.on('data', (data) => {
-      bodyChunks.push(data);
-  });
-  req.on('end', () => {
-      body = Buffer.concat(bodyChunks);
-      data = JSON.parse(body);
-      if (data.auth && data.auth.token == "120987") {
-        send_data(data);
-          // console.log(data.teams);
-      }
-      res.end('');
-  });
-});
-cs_listener.listen("1337");
+// CS JSON listener
+// const http2 = require('http'),
+// cs_listener = http2.createServer((req, res) => {
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   if (req.method != "POST") {
+//       return res.end('');
+//   }
+//   let body,
+//       data,
+//       bodyChunks = [];
+//   req.on('data', (data) => {
+//       bodyChunks.push(data);
+//   });
+//   req.on('end', () => {
+//       body = Buffer.concat(bodyChunks);
+//       data = JSON.parse(body);
+//       if (data.auth && data.auth.token == "120987") {
+//         send_data(data);
+//           // console.log(data.teams);
+//       }
+//       res.end('');
+//   });
+// });
+// cs_listener.listen("1337");
 
 
 
